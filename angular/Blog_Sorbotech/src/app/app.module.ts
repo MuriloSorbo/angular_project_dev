4c2b9d887477a76b12e8bed66e2ef1d48fa8f0a1
Murilo Sorbo <developermurilo@gmail.com>
2022-06-20 21:43:09 -0300

    Mais um dia na caminhada

---
+++ angular/Blog_Sorbotech/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { RoutingModule } from './app.router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PostPageComponent } from './pages/post-page/post-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    PostPageComponent,
+    ErrorPageComponent
+  ],
+  imports: [
+    RoutingModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
